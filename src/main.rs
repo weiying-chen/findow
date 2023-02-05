6daefdde76652efb682f4f19b112953c0976221a
Alex <alexchen.info@gmail.com>
2023-02-05 23:36:13 +0800

    Add `window_activate`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use glib_macros::clone;
 use gtk::glib;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Orientation};
+use gtk::{Application, ApplicationWindow, Orientation};
 use std::process::Command;
 
 fn main() {
@@ -14,7 +14,6 @@ fn main() {
 }
 
 fn run_command(command: &str) -> String {
-    // let cmd = format!("{} {}", command, input_text);
     let output = Command::new("sh")
         .arg("-c")
         .arg(command)
@@ -31,13 +30,6 @@ fn run_command(command: &str) -> String {
 fn build_ui(app: &Application) {
     let input = gtk::Entry::builder()
         .placeholder_text("input")
-        .margin_top(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    let button = Button::builder()
-        .label("Submit")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
@@ -56,15 +48,8 @@ fn build_ui(app: &Application) {
         .build();
 
     gtk_box.append(&input);
-    gtk_box.append(&button);
 
     input.connect_activate(clone!(@weak window => move |entry| {
-        // TODO: This could be a function
-
-        // "xdotool search --onlyvisible --name {}",
-        // "xdotool getwindowname {}",
-        // "xprop -id {} | grep WM_CLASS",
-
         let input_text = entry.text();
         let command = format!("xdotool search --onlyvisible --name {}", input_text);
         let window_id_output = run_command(&command);
@@ -80,6 +65,12 @@ fn build_ui(app: &Application) {
         let window_class_output = run_command(&command);
 
         println!("window_class: {}", window_class_output);
+
+        // `xdotool windowactivate` doesn't produce any output
+        let command = format!("xdotool windowactivate {}", window_id_output);
+        let window_activate_output = run_command(&command);
+
+        println!("window_activate: {}", window_activate_output);
         window.close();
     }));
 
