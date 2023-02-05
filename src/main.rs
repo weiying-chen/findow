1fdb858130469d8192e1ffccce1269764117eb33
Alex <alexchen.info@gmail.com>
2023-02-06 00:21:00 +0800

    Use window.hide()

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,6 @@ fn main() {
     app.run();
 }
 
-fn run_command(command: &str) -> String {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(command)
-        .output()
-        .expect("Failed to execute proccess.");
-
-    if output.status.success() {
-        String::from_utf8_lossy(&output.stdout).trim().to_owned()
-    } else {
-        String::from_utf8_lossy(&output.stderr).trim().to_owned()
-    }
-}
-
 fn build_ui(app: &Application) {
     let input = gtk::Entry::builder()
         .placeholder_text("input")
@@ -51,28 +37,18 @@ fn build_ui(app: &Application) {
 
     input.connect_activate(clone!(@weak window => move |entry| {
         let input_text = entry.text();
-        let command = format!("xdotool search --onlyvisible --name {}", input_text);
-        let window_id_output = run_command(&command);
 
-        println!("window_id: {}", window_id_output);
+        let output = Command::new("wmctrl")
+            .arg("-a")
+            .arg(input_text)
+            .output()
+            .expect("Failed to execute wmctrl command");
 
-        let command = format!("xdotool getwindowname {}", window_id_output);
-        let window_name_output = run_command(&command);
+        println!("output.stdout: {}", String::from_utf8_lossy(&output.stdout));
 
-        println!("window_name: {}", window_name_output);
-
-        let command = format!("xprop -id {} | grep WM_CLASS", window_id_output);
-        let window_class_output = run_command(&command);
-
-        println!("window_class: {}", window_class_output);
-
-        // `xdotool windowactivate` doesn't produce any output
-        let command = format!("xdotool windowactivate {}", window_id_output);
-        let window_activate_output = run_command(&command);
-
-        println!("window_activate: {}", window_activate_output);
-        window.close();
+        // `window.close()` sometimes won't bring the matched window to the front.
+        window.hide();
     }));
 
-    window.present();
+    window.show();
 }
