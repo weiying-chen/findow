0efdd671029983fcef291a5c994a11f92ab6e2d2
Alex <alexchen.info@gmail.com>
2023-02-11 19:22:16 +0800

    Add `window_name` to loop

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,6 @@ fn print_output(name: &str, output: &Output) {
 }
 
 fn build_ui(app: &Application) {
-    let list_box_row = ListBoxRow::new();
-    let label = Label::new(Some("83823"));
-    let list_box = ListBox::new();
-
-    list_box_row.add(&label);
-    list_box.add(&list_box_row);
-
     let vbox = Box::new(gtk::Orientation::Vertical, 10);
 
     let input = gtk::Entry::builder()
@@ -46,6 +39,8 @@ fn build_ui(app: &Application) {
         .margin_start(12)
         .margin_end(12)
         .build();
+
+    let list_box = ListBox::new();
 
     vbox.pack_start(&input, false, false, 0);
     vbox.pack_start(&list_box, true, true, 0);
@@ -76,44 +71,23 @@ fn build_ui(app: &Application) {
             list_box.remove(&row)
         }
 
-        let label = Label::new(Some("231312"));
-        let list_box_row = ListBoxRow::new();
+        for window_id in window_id_output_string.split("\n") {
+            let command = format!("xdotool getwindowname {}", window_id);
+            let window_name_output = run_command(&command);
+            let window_name = String::from_utf8_lossy(&window_name_output.stdout).to_string();
 
-        list_box_row.add(&label);
-        list_box.add(&list_box_row);
+            let label = Label::new(Some(&window_name));
+            let list_box_row = ListBoxRow::new();
+
+            list_box_row.add(&label);
+            list_box.add(&list_box_row);
+        }
 
         // This is necessary because here `list_box_row` has been added dynamically.
         list_box.show_all();
 
-        // let window_id_array: Vec<&str> = window_id_output_string.split("\n").collect();
-
-        // for window_id in window_id_array {
-        //     let label = Label::new(Some(window_id));
-        //     let list_box_row = ListBoxRow::new();
-
-        //     list_box_row.add(&label);
-        //     list_box.add(&list_box_row);
-        // }
-
         // TODO: Check the result of the split
         // See if can use filter like suggested in Reddit
-
-        // for window_id in window_id_output_string.split("\n") {
-        //     if !window_id.is_empty() {
-        //         // TODO: Should fix this because because it fails when `window_id_output` has two ids.
-        //         let command = format!("xdotool getwindowname {}", window_id);
-        //         let window_name_output = run_command(&command);
-        //         let window_name = String::from_utf8_lossy(&window_name_output.stdout).to_string();
-        //         let label = Label::new(None);
-
-        //         label.set_text(&window_name);
-
-        //         let list_box_row = ListBoxRow::new();
-
-        //         list_box_row.add(&label);
-        //         list_box.add(&list_box_row);
-        //     }
-        // }
     });
 
     let window_id_output_clone = Rc::clone(&window_id_output_rc);
